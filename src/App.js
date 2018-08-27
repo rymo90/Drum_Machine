bf13138bb04ff26b519d6cc2876c98508da04596
rymo90 <red.01yassin@gmail.com>
2018-08-27 23:37:28 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,62 +2,81 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import './App.css';
 
-const data = [
-  { id: 'snare', letter: 'Q', src: 'https://www.myinstants.com/media/sounds/snare.mp3' },
-  { id: 'bass 1', letter: 'W', src: 'https://www.myinstants.com/media/sounds/bass-drum.mp3' },
-  { id: 'bongo', letter: 'E', src: 'http://tipiwiki.free.fr/snd/Percussion(4e)2.wav' },
-  { id: 'tom tom', letter: 'A', src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav' },
-  { id: 'bass 3', letter: 'S', src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' },
-  { id: 'shotgun', letter: 'D', src: 'http://david.guerrero.free.fr/Effects/ShotgunReload.wav' },
-  { id: 'high hat', letter: 'Z', src: 'http://www.denhaku.com/r_box/tr707/closed.wav' },
-  { id: 'drum hit', letter: 'X', src: 'http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3' },
-  { id: 'laser', letter: 'C', src: 'http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav'  },
+const data = [{
+    id: 'snare',
+    letter: 'Q',
+    src: 'https://www.myinstants.com/media/sounds/snare.mp3'
+  },
+  {
+    id: 'bass 1',
+    letter: 'W',
+    src: 'https://www.myinstants.com/media/sounds/bass-drum.mp3'
+  },
+  {
+    id: 'bongo',
+    letter: 'E',
+    src: 'http://tipiwiki.free.fr/snd/Percussion(4e)2.wav'
+  },
+  {
+    id: 'tom tom',
+    letter: 'A',
+    src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav'
+  },
+  {
+    id: 'bass 3',
+    letter: 'S',
+    src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav'
+  },
+  {
+    id: 'shotgun',
+    letter: 'D',
+    src: 'http://david.guerrero.free.fr/Effects/ShotgunReload.wav'
+  },
+  {
+    id: 'high hat',
+    letter: 'Z',
+    src: 'http://www.denhaku.com/r_box/tr707/closed.wav'
+  },
+  {
+    id: 'drum hit',
+    letter: 'X',
+    src: 'http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3'
+  },
+  {
+    id: 'laser',
+    letter: 'C',
+    src: 'http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav'
+  },
 ]
-class Drum extends React.Component{
-    handleClick= ()=>{
-    this.audio.play()
-    this.audio.currentTime=0
-  }
-  render(){
-    return(
-    <div className="drum-pad"
-      id={this.props.id}
-      onClick = {this.handleClick}
-      >
-        <p>{this.props.letter}</p>
-        <audio
-          ref= {ref => this.audio = ref}
-          className="clip"
-          id={this.props.id}
-          src={this.props.src}>
-        </audio>
-      </div>
-    )
-  }
+
+const Drum = props => {
+  const alphabet= ["Q","W","E","A","S","D","Z","X","C"]
+  const drum = alphabet.map((data, index) => {
+    return <button  class="drum-pad" id = {data} >
+      <audio class="clip" id= {data} src="#"></audio>
+      <h1>{data}</h1>
+    </button>
+  })
+
+  return (
+    <div>
+      {drum}
+    </div>
+  )
 }
 class App extends React.Component{
   constructor(props){
-    super(props)
+    super(props);
   }
-
-
   render() {
     return (
-      <div id ="drum-machine" >
-        <h1 id="display"></h1>
-        {data.map(d =>(
-          <Drum
-            id={d.id}
-            letter={d.letter}
-            src= {d.src}
-            onClick = {this.handleClick}
+      <div className="App">
+        <div id="drum-machine">
+          <h2 id="display">DrumMachinReact</h2>
+          <Drum />
+        </div>
 
-            />
-
-
-        ))}
       </div>
     )
   }
 }
-export default App;
